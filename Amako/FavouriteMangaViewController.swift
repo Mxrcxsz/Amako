0fba552e80942430332087c9389f3647be2a7bba
Amosymh <amosyeongmh@gmail.com>
2022-01-27 23:18:05 +0800

    merge error just now, created favourite manga controller again

--- Amako/FavouriteMangaViewController.swift
+++ Amako/FavouriteMangaViewController.swift
@@ -10,22 +10,40 @@ import UIKit
 private let reuseIdentifier = "Cell"
 
 class FavouriteMangaViewController: UICollectionViewController {
-    //test this
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
         // Register cell classes
         self.collectionView!.register(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
     }
-    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    // MARK: UICollectionViewDataSource
+
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 1
+        return 0
     }
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 12
+        return 0
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -34,10 +52,6 @@ class FavouriteMangaViewController: UICollectionViewController {
         // Configure the cell
     
         return cell
-    }
-    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        collectionView.deselectItem(at: indexPath, animated: true)
-        print("tapped")
     }
 
     // MARK: UICollectionViewDelegate
